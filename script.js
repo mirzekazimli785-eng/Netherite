d65a8f59f96ea5cf39e22c9668e8e9b41ff7a0c7
mirzekazimli785-eng <mirzekazimli785@gmail.com>
2026-05-04 14:51:36 +0400

    Create kitIcons object in script.js
    Added kitIcons object with item-image mappings.

---
+++ script.js
@@ -0,0 +1,13 @@
+const kitIcons = {
+    "NethPot": "1000189822.jpg",
+    "Sword": "1000189824.jpg",
+    "Axe": "1000189826.jpg",
+    "Uhc": "1000189828.jpg",
+    "Mace": "1000189830.jpg",
+    "Potion": "1000189832.jpg",
+    "Crystal": "1000189834.jpg",
+    "SMP": "1000189836.jpg"
+};
+
+// Digər sıralama və tier məntiqləri bura əlavə olunacaq...
+
